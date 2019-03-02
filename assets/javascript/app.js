41e8d7b61c873f6a4d7c3ef2a30cd0d76f98b29f
Nash McDonald <nash42192@gmail.com>
2019-03-02 12:22:37 -0800

    did the thing

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDjdEebUvzNdlaDLR5ZaIKMkeycWZ-wJM8",
+  authDomain: "employeedatamanagement-deaf5.firebaseapp.com",
+  databaseURL: "https://employeedatamanagement-deaf5.firebaseio.com",
+  projectId: "employeedatamanagement-deaf5",
+  storageBucket: "employeedatamanagement-deaf5.appspot.com",
+  messagingSenderId: "859236025483"
+};
+firebase.initializeApp(config);
+database = firebase.database();
+
+$(document).ready(function () {
+
+    $('.btn').on('click', function(event){
+
+        event.preventDefault();
+
+        var name = $('#name-input').val().trim();
+        var role = $('#role-input').val().trim();
+        var date = $('#date-input').val().trim();
+        var rate = $('#rate-input').val().trim();
+
+        $('#name-input').val('');
+        $('#role-input').val('');
+        $('#date-input').val('');
+        $('#rate-input').val('');
+
+        
+
+        console.log(name);
+        console.log(role);
+        console.log(date);
+        console.log(rate);
+
+        database.ref().push({
+            name: name,
+            role: role,
+            date: date,
+            rate: rate,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+  
+    });
+
+    addInfo();
+
+});
+
+function addInfo(){
+    database.ref().on('child_added', function(snapshot) {
+
+        var sv = snapshot.val();
+
+        var tMonths = sv.dateAdded - sv.date;
+        console.log(tMonths);
+
+        console.log(sv.name);
+        console.log(sv.role);
+        console.log(sv.date);
+        console.log(sv.rate);
+        console.log(sv.dateAdded);
+
+        var row = $('<tr>');
+        var tName = $('<td>').text(sv.name);
+        var tRole = $('<td>').text(sv.role);
+        var tDate = $('<td>').text(sv.date);
+        var tRate = $('<td>').text(sv.rate);
+
+        
+        row.append(tName).append(tRole).append(tDate).append(tRate);
+        $('#table').append(row);
+    })
+}
